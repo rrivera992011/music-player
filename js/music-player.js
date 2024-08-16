13a6e99ec3b6833542881e55104dd0b08d565bd8
Rafael <rrivera992011@hotmail.com>
2024-08-15 23:23:11 -0500

    Starting JQuery implementation

--- js/music-player.js
+++ js/music-player.js
@@ -1,41 +1,85 @@
-let progress = document.getElementById("progress");
-let song = document.getElementById("song");
-let controlIcon = document.getElementById("controlIcon");
+// let progress = document.getElementById("progress");
+// let song = document.getElementById("song");
+// let controlIcon = document.getElementById("controlIcon");
 
-song.onloadedmetadata = function () {
-  progress.max = song.duration;
-  progress.value = song.currentTime;
-};
+// song.onloadedmetadata = function () {
+//   progress.max = song.duration;
+//   progress.value = song.currentTime;
+// };
 
-function playPause() {
-  if (controlIcon.classList.contains("fa-pause")) {
-    song.pause();
-    controlIcon.classList.remove("fa-pause");
-    controlIcon.classList.add("fa-play");
-  } else {
-    song.play();
-    controlIcon.classList.add("fa-pause");
-    controlIcon.classList.remove("fa-play");
-  }
-}
+// function playPause() {
+//   if (controlIcon.classList.contains("fa-pause")) {
+//     song.pause();
+//     controlIcon.classList.remove("fa-pause");
+//     controlIcon.classList.add("fa-play");
+//   } else {
+//     song.play();
+//     controlIcon.classList.add("fa-pause");
+//     controlIcon.classList.remove("fa-play");
+//   }
+// }
 
-function rewind() {
-  song.currentTime -= 5;
-}
+// function rewind() {
+//   song.currentTime -= 5;
+// }
 
-function fastForward() {
-  song.currentTime += 5;
-}
+// function fastForward() {
+//   song.currentTime += 5;
+// }
 
-if (song.play()) {
-  setInterval(() => {
-    progress.value = song.currentTime;
-  }, 500);
-}
+// if (song.play()) {
+//   setInterval(() => {
+//     progress.value = song.currentTime;
+//   }, 500);
+// }
 
-progress.onchange = function () {
-  song.play();
-  song.currentTime = progress.value;
-  controlIcon.classList.add("fa-pause");
-  controlIcon.classList.remove("fa-play");
-};
+// progress.onchange = function () {
+//   song.play();
+//   song.currentTime = progress.value;
+//   controlIcon.classList.add("fa-pause");
+//   controlIcon.classList.remove("fa-play");
+// };
+
+$(document).ready(function () {
+  var controlIcon = $("#controlIcon");
+  var song = $("#song");
+  var progress = $("#progress");
+  var rewindIcon = $("#rewindIcon");
+  var forwardIcon = $("#forwardIcon");
+
+  song.on("loadedmetadata", function () {
+    progress.max = song[0].duration;
+    progress.value = song[0].currentTime;
+  });
+
+  controlIcon.click(function () {
+    if (controlIcon.hasClass("fa-pause")) {
+      song[0].pause();
+      controlIcon.removeClass("fa-pause").addClass("fa-play");
+    } else {
+      song[0].play();
+      controlIcon.removeClass("fa-play").addClass("fa-pause");
+    }
+  });
+
+  rewindIcon.click(function () {
+    song[0].currentTime -= 5;
+  });
+
+  forwardIcon.click(function () {
+    song[0].currentTime += 5;
+  });
+
+  // if (song[0].play()) {
+  //   setInterval(() => {
+  //     progress.value = song[0].currentTime;
+  //   }, 500);
+  // }
+
+  // progress.onchange = function () {
+  //   song.play();
+  //   song.currentTime = progress.value;
+  //   controlIcon.classList.add("fa-pause");
+  //   controlIcon.classList.remove("fa-play");
+  // };
+});
